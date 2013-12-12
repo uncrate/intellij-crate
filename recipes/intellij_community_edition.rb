ea0b35f6c8c9ea0ab2358b0557adbe7be5e31b9f
John Leacox <john.leacox@cerner.com>
2013-12-12 13:07:53 -0600

    Update to 13

--- recipes/intellij_community_edition.rb
+++ recipes/intellij_community_edition.rb
@@ -2,8 +2,6 @@ return if skip_unsupported_platform
 
 intellij_tar_filename = File.basename(node['intellij_community_edition']['download_url'])
 intellij_tar_filepath = "#{Chef::Config['file_cache_path']}/#{intellij_tar_filename}"
-intellij_extract_path = "#{Chef::Config['file_cache_path']}/intellij/#{node['intellij_community_edition']['checksum']}"
-intellij_install_path = "#{node['intellij_community_edition']['install_path']}/#{node['intellij_community_edition']['install_dir']}"
 
 case node[:platform]
 when "centos", "redhat", "debian", "ubuntu"
@@ -12,7 +10,6 @@ when "centos", "redhat", "debian", "ubuntu"
   ark node['intellij_community_edition']['install_dir'] do
     url node['intellij_community_edition']['download_url']
     path node['intellij_community_edition']['install_path']
-    checksum node['intellij_community_edition']['checksum']
     mode 0755
     action :put
   end
