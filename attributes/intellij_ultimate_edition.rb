4562fe41839b7318420be68f5368aae953656efd
John Leacox <johnlcox@gmail.com>
2014-01-18 19:45:23 -0600

    Update ultimate edition recipe to be like community edition

--- attributes/intellij_ultimate_edition.rb
+++ attributes/intellij_ultimate_edition.rb
@@ -1,7 +1,15 @@
-default['intellij_ultimate_edition']['download_url']="http://download.jetbrains.com/idea/ideaIU-13beta.tar.gz"
-default['intellij_ultimate_edition']['checksum']="e93e11876b2d11e66590429f954233737092527d07ff30148655ca74a1d1ef31"
+case node[:platform]
+when "centos", "redhat", "debian", "ubuntu"
+  default['intellij_ultimate_edition']['download_url']="http://download.jetbrains.com/idea/ideaIU-13.0.1.tar.gz"
+
+  default['intellij_ultimate_edition']['name']="IntelliJ IDEA 13 IU"
+when "mac_os_x"
+  default['intellij_ultimate_edition']['download_url']="http://download.jetbrains.com/idea/ideaIU-13.0.1.dmg"
+
+  default['intellij_ultimate_edition']['name']="IntelliJ IDEA 13 IU"
+end
+
 default['intellij_ultimate_edition']['install_path']="/usr/local"
 default['intellij_ultimate_edition']['install_dir']="ideaIU-13"
 
-default['intellij_ultimate_edition']['name']="IntelliJ IDEA Ultimate Edition"
 default['intellij_ultimate_edition']['shortcut_name']="intellij-ideaIU"
--- recipes/intellij_ultimate_edition.rb
+++ recipes/intellij_ultimate_edition.rb
@@ -1,21 +1,29 @@
+return if skip_unsupported_platform
+
 intellij_tar_filename = File.basename(node['intellij_ultimate_edition']['download_url'])
 intellij_tar_filepath = "#{Chef::Config['file_cache_path']}/#{intellij_tar_filename}"
-intellij_extract_path = "#{Chef::Config['file_cache_path']}/intellij/#{node['intellij_ultimate_edition']['checksum']}"
-intellij_install_path = "#{node['intellij_ultimate_edition']['install_path']}/#{node['intellij_ultimate_edition']['install_dir']}"
 
-include_recipe "ark"
+case node[:platform]
+when "centos", "redhat", "debian", "ubuntu"
+  include_recipe "ark"
 
-ark node['intellij_ultimate_edition']['install_dir'] do
-  url node['intellij_ultimate_edition']['download_url']
-  path node['intellij_ultimate_edition']['install_path']
-  checksum node['intellij_ultimate_edition']['checksum']
-  owner node['uncrate']['user']
-  group node['uncrate']['user']
-  mode 0755
-  action :put
-end
+  ark node['intellij_ultimate_edition']['install_dir'] do
+    url node['intellij_ultimate_edition']['download_url']
+    path node['intellij_ultimate_edition']['install_path']
+    mode 0755
+    action :put
+  end
 
-template "/usr/share/applications/#{node['intellij_ultimate_edition']['shortcut_name']}.desktop" do
-  source "intellij_ultimate_edition.desktop.erb"
-  mode "0644"
+  template "/usr/share/applications/#{node['intellij_ultimate_edition']['shortcut_name']}.desktop" do
+    source "intellij_ultimate_edition.desktop.erb"
+    mode "0644"
+  end
+when "mac_os_x"
+  include_recipe "dmg"
+
+  dmg_package node['intellij_ultimate_edition']['name'] do
+      source node['intellij_ultimate_edition']['download_url']
+      owner node['uncrate']['user']
+      action :install
+    end
 end
