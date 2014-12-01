c369c23add30641ad55076195cd730e572f59d60
John Leacox <johnlcox@users.noreply.github.com>
2014-12-01 09:49:40 -0600

    Update to Community Edition 14.0.1

--- attributes/intellij_community_edition.rb
+++ attributes/intellij_community_edition.rb
@@ -1,15 +1,15 @@
 case node[:platform]
 when "centos", "redhat", "debian", "ubuntu"
-  default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-13.1.tar.gz"
+  default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-14.0.1.tar.gz"
 
-  default['intellij_community_edition']['name']="IntelliJ IDEA 13 CE"
+  default['intellij_community_edition']['name']="IntelliJ IDEA 14 CE"
 when "mac_os_x"
-  default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-13.1.dmg"
+  default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-14.0.1.dmg"
 
-  default['intellij_community_edition']['name']="IntelliJ IDEA 13 CE"
+  default['intellij_community_edition']['name']="IntelliJ IDEA 14 CE"
 end
 
 default['intellij_community_edition']['install_path']="/usr/local"
-default['intellij_community_edition']['install_dir']="ideaIC-13"
+default['intellij_community_edition']['install_dir']="ideaIC-14"
 
 default['intellij_community_edition']['shortcut_name']="intellij-ideaIC"
