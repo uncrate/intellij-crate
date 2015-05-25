90c475d0511365a05d974d85cc22fcdef972db4c
John Leacox <johnlcox@users.noreply.github.com>
2015-05-25 09:22:14 -0500

    Update default community version to 14.1.3

--- attributes/intellij_community_edition.rb
+++ attributes/intellij_community_edition.rb
@@ -1,10 +1,10 @@
 case node[:platform]
 when "centos", "redhat", "debian", "ubuntu"
-  default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-14.0.1.tar.gz"
+  default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-14.1.3.tar.gz"
 
   default['intellij_community_edition']['name']="IntelliJ IDEA 14 CE"
 when "mac_os_x"
-  default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-14.0.1.dmg"
+  default['intellij_community_edition']['download_url']="http://download.jetbrains.com/idea/ideaIC-14.1.3.dmg"
 
   default['intellij_community_edition']['name']="IntelliJ IDEA 14 CE"
 end
